ae21f455b5da69c822906a5ddcab488fbbb3e818
Kemirdin <qemirdin@gmail.com>
2017-12-22 23:04:51 +0100

    Understanding Uninitialized Variables

---
+++ Basic-JavaScript-freeCodeCamp/5-UninitialVariables.js
@@ -0,0 +1,13 @@
+// Initialize these three variables
+var a;
+var b;
+var c;
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + ' String!';
+var a = 6;
+var b = 15;
+var c = 'I am a String!';
