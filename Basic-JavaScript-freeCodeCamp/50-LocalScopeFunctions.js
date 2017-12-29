e8e0357659fe7e8588b0e6133a81cf53a17d083b
Kemirdin <qemirdin@gmail.com>
2017-12-29 22:29:27 +0100

    Local Scope and Functions

---
+++ Basic-JavaScript-freeCodeCamp/50-LocalScopeFunctions.js
@@ -0,0 +1,10 @@
+function myLocalScope () {
+  'use strict';
+  var myVar = 'Hello';
+
+  console.log (myVar);
+}
+myLocalScope ();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
