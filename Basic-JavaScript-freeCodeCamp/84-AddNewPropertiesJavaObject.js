1abfc5827919f64508ba57c1e9551bf9bebc2a15
Kemirdin <qemirdin@gmail.com>
2018-01-06 20:41:26 +0100

    Add New Properties to a JavaScript Object

---
+++ Basic-JavaScript-freeCodeCamp/84-AddNewPropertiesJavaObject.js
@@ -0,0 +1,42 @@
+// Example
+var ourDog = {
+  name: 'Camper',
+  legs: 4,
+  tails: 1,
+  friends: ['everything!'],
+};
+
+ourDog.bark = 'bow-wow';
+
+// Setup
+var myDog = {
+  name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['Free Code Camp Campers'],
+};
+
+// Only change code below this line.
+Answers;
+// Example
+var ourDog = {
+  name: 'Camper',
+  legs: 4,
+  tails: 1,
+  friends: ['everything!'],
+};
+
+ourDog.bark = 'bow-wow';
+
+// Setup
+var myDog = {
+  name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['Free Code Camp Campers'],
+};
+
+// Only change code below this line.
+
+myDog.bark = 'woof'; // Dot notation
+myDog['bark'] = 'woof'; // Bracket notation
